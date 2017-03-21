363ce1e13ede0ffe503632ac7be6b08796f4a328
Jenny Zhang <jz.skittles@gmail.com>
2017-03-21 08:36:07 -0500

    changed register

--- app/src/main/java/technovations/ajuj/technovations2017/Register.java
+++ app/src/main/java/technovations/ajuj/technovations2017/Register.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,13 +32,13 @@ import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
-    private EditText username, password, first_name, last_name, email, year;
+    private EditText username, password, first_name, last_name, email, org_name, address, phoneNumber;
     private TextView guideline;
     private Button register, back;
+    private CheckBox donor, receiver;
     private RequestQueue requestQueue;
-
     private StringRequest request;
-
+    private String DorR;
     private String URL = "";
 
     @Override
@@ -49,8 +50,16 @@ public class Register extends AppCompatActivity {
         password = (EditText) findViewById(R.id.password_register);
         first_name = (EditText) findViewById(R.id.first_name_register);
         last_name = (EditText) findViewById(R.id.last_name_register);
+        org_name = (EditText) findViewById(R.id.org_name_register);
         email = (EditText) findViewById(R.id.email_register);
-        year = (EditText) findViewById(R.id.year_register);
+        address = (EditText) findViewById(R.id.address_register);
+        phoneNumber = (EditText) findViewById(R.id.phone_number_register);
+        donor = (CheckBox) findViewById(R.id.donor_register);
+        receiver = (CheckBox) findViewById(R.id.receiver_register);
+        if(donor.isChecked())
+            DorR = "donor";
+        else
+            DorR = "receiver";
 
         register = (Button) findViewById(R.id.register_register);
         back = (Button) findViewById(R.id.back_register);
@@ -63,7 +72,7 @@ public class Register extends AppCompatActivity {
             @Override
             public void onClick(View view){
 
-                request = new StringRequest(Request.Method.POST, "http://ajuj.comlu.com/register.php", new Response.Listener<String>() {
+                request = new StringRequest(Request.Method.POST, "https://2017ajuj.000webhostapp.com/register.php", new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         // Toast.makeText(getApplicationContext(),response,Toast.LENGTH_SHORT).show();
@@ -94,7 +103,11 @@ public class Register extends AppCompatActivity {
                         hashMap.put("first",first_name.getText().toString());
                         hashMap.put("last",last_name.getText().toString());
                         hashMap.put("email",email.getText().toString());
-                        hashMap.put("year", year.getText().toString());
+                        hashMap.put("orgname", org_name.getText().toString());
+                        hashMap.put("address", address.getText().toString());
+                        hashMap.put("phoneNumber", phoneNumber.getText().toString());
+                        hashMap.put("DorR", DorR);
+
 
                         return hashMap;
                     }
